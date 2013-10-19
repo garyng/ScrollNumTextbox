e5f16fbf222a25110b3536e5c13d348e6ef38088
GaryNg <garyng.zhongbo@gmail.com>
2013-10-19 17:20:34 +0800

    Done basic function

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ScrollNumTextbox
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.scrollNumText1 = new System.Windows.Forms.ScrollNumText();
+			this.SuspendLayout();
+			// 
+			// scrollNumText1
+			// 
+			this.scrollNumText1.Location = new System.Drawing.Point(102, 58);
+			this.scrollNumText1.Name = "scrollNumText1";
+			this.scrollNumText1.ShortcutsEnabled = false;
+			this.scrollNumText1.Size = new System.Drawing.Size(100, 20);
+			this.scrollNumText1.TabIndex = 0;
+			this.scrollNumText1.Text = "0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(290, 269);
+			this.Controls.Add(this.scrollNumText1);
+			this.Name = "Form1";
+			this.Text = "Test";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ScrollNumText scrollNumText1;
+	}
+}
+
---
+++ ScrollNumTextControl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms
+{
+	class ScrollNumText : TextBox
+	{
+		enum Direction
+		{
+			Vertical,
+			Horizontal,
+			None
+		}
+
+		public ScrollNumText()
+		{
+
+			Direction curDir = new Direction();
+			Point pDown = new Point(-1, -1);
+			Point? pPre = null;
+			this.ShortcutsEnabled = false;
+			this.MouseDown += delegate(object s, MouseEventArgs ev)
+			{
+				pDown = ev.Location;
+			};
+
+			this.MouseUp += delegate(object s, MouseEventArgs ev)
+			{
+				pDown = new Point(-1, -1);
+				curDir = Direction.None;
+			};
+
+			this.MouseMove += delegate(object s, MouseEventArgs ev)
+			{
+				Console.WriteLine(curDir.ToString());
+				if (pDown.X >= 0 || pDown.Y >= 0)
+				{
+					long num;
+					long.TryParse(this.Text, out num);
+					bool isBackward = false;
+					if (Math.Abs((ev.Location.X - pDown.X)) > 10 && (ev.Location.X - pDown.X) % 5 == 0)
+					{
+						if (curDir == Direction.None)
+						{
+							curDir = Direction.Vertical;
+						}
+						if (curDir == Direction.Vertical)
+						{
+							if (pPre.HasValue && (ev.Location.X - pPre.Value.X) < 0)
+							{
+								isBackward = true;
+							}
+							if ((ev.Location.X - pDown.X) > 0 && !isBackward)
+							{
+								num += 1;
+							}
+							else if ((ev.Location.X - pDown.X) < 0 || isBackward)
+							{
+								num -= 1;
+							}
+						}
+					}
+					else if (Math.Abs((ev.Location.Y - pDown.Y)) > 10 && (ev.Location.Y - pDown.Y) % 5 == 0)
+					{
+						if (curDir == Direction.None)
+						{
+							curDir = Direction.Horizontal;
+						}
+						if (curDir == Direction.Horizontal)
+						{
+							if (pPre.HasValue && (ev.Location.Y - pPre.Value.Y) > 0)
+							{
+								isBackward = true;
+							}
+							if ((ev.Location.Y - pDown.Y) < 0 && !isBackward)
+							{
+								num += 1;
+							}
+							else if ((ev.Location.Y - pDown.Y) > 0 || isBackward)
+							{
+								num -= 1;
+							}
+						}
+					}
+					pPre = ev.Location;
+					this.Text = num.ToString();
+				}
+			};
+
+			this.KeyDown += delegate(object s, KeyEventArgs ev)
+			{
+				Console.WriteLine((char)ev.KeyCode);
+				if (!(ev.KeyCode == Keys.Back || char.IsDigit((char)ev.KeyCode)))
+				{
+					ev.SuppressKeyPress = true;
+				}
+			};
+		}
+
+		public override string Text
+		{
+			get
+			{
+				return base.Text;
+			}
+			set
+			{
+				long num;
+				if (long.TryParse(base.Text, out num))
+				{
+					base.Text = value;
+				}
+				else
+				{
+					base.Text = "0";
+				}
+
+			}
+		}
+	}
+
+}
